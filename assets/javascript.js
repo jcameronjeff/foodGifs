4c761402e37db23a0f13db9dfae810e0321954e4
jcameronjeff <jcameronjeff@gmail.com>
2018-01-09 15:48:50 -0500

    newstuff

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,46 +1,22 @@
-////### Before You Begin
-//
-//	1. **Hit the GIPHY API**.
-//	   * Fool around with the GIPHY API. [Giphy API](https://github.com/Giphy).
-//	   * Be sure to read about these GIPHY parameters (hint, hint):
-//		 * `q`
-//		 * `limit`
-//		 * `rating`
-//	   * Like many APIs, GIPHY requires developers to use a key to access their API data. To use the GIPHY API, you'll need a GIPHY account (don't worry, it's free!) and then obtain an API Key by [creating an app](https://developers.giphy.com/dashboard/?create=true).
-//	   * Make sure you switch the protocol in the query URL from **`http to https`**, or the app may not work properly when deployed to Github Pages.
-//
-
-//   
-//	
-//		   
-//	For each loop that appends button with topic "name". 
-//		   
-//	click listener that grabs 10 non animated gif images. When each button is clicked, they animate. CLick again and they stop.
-//		   
-//	display giphy rating under each 
-//		   
-
-//https://api.giphy.com/v1/gifs/search?api_key=uIHPgaW6SucV93Vl5Z2yxoIrXHU51Vyz&q=
 
 
 var cuisines = ["italian", "mexican", "thai", "chinese", "sushi", "japanese", "vietnamese", ];
 
 
-
    function renderButtons() {
-	   	
+
 	   var buttons = $("<div class='buttons'>");
         // Deleting the movies prior to adding new movies
         // (this is necessary otherwise you will have repeat buttons)
-     
+
 
         // Looping through the array of movies
         for (var i = 0; i < cuisines.length; i++) {
 
-         
+
           var a = $("<button>");
           // Adding a class of movie to our button
-          a.addClass("cuisine");
+          a.addClass("cuisine btn-primary");
           // Adding a data-attribute
           a.attr("data-name", cuisines[i]);
           // Providing the initial button text
@@ -53,60 +29,87 @@ var cuisines = ["italian", "mexican", "thai", "chinese", "sushi", "japanese", "v
 renderButtons()
 
 function displayGifs() {
-	  
+
 	$("#foodPorn").empty();
-		
+
 	var topic = $(this).attr("data-name");
-	
+
 	console.log(topic);
-	
+
 	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uIHPgaW6SucV93Vl5Z2yxoIrXHU51Vyz&q=" + topic + "+food&limit=10";
-	
-	
-	
+
+
+
 		$.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-		
+
 		// Creating a div to view the gallery.
-			
+
 		var foodView = $("<div class='foodView'>");
-		
+
 		var gifData = response.data;
-		
-			
+
+
 		//gets the image rating
 		 for (var i = 0; i < response.data.length; i++) {
 
 			 	var imageId = gifData[i].id;
-			 
+
 			 	var preview = gifData[i].images.fixed_height_still.url;
-			 
-			 	console.log(preview);	
-			 
-			 	var prev = $("<img>").attr({ src: preview, id: imageId } )
-				foodView.append(prev);
-	
-			 	var animated = gifData.id.images.fixed_height_downsampled.url;
-			 
-			 
-			 
-			 
-			 //gets rating and appends to //div
+
+			 	var animated = gifData[i].images.fixed_height.url;
+//			 	var animated =
 			 	var rated = gifData[i].rating;
-			 
-		       	var pOne = $("<p>").text("Rating: " + rated);
-			
+
+			 	var pOne = $("<p>").text("Rating: " + rated).attr({class: "foodCap"});
+
+
+			 	var prev = $("<img>").attr({ src: preview, alt: animated, class: "foodImg" });
+
+				pOne.append(prev);
 				foodView.append(pOne);
-          
+//
         	}
-      	
-		
+
 		 $("#foodPorn").append(foodView);
-			});
-					
+				});
+
 }
 
+$("#addFood").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var newFood = $("#foodInput").val().trim();
+        var newButton = $("<button>");
 
- $(document).on("click", ".cuisine", displayGifs);
+        // Adding movie from the textbox to our array
+
+        // Adding a class of movie to our button
+        newButton.addClass("cuisine btn-primary");
+        // Adding a data-attribute
+        newButton.attr("data-name", newFood);
+        // Providing the initial button text
+        newButton.text(newFood);
+        // Adding the button to the buttons-view div
+        $("#buttonsView").append(newButton);
+
+      });
+
+
+
+// animates play pause by switching alt with src and vice versa.
+
+function animate() {
+	var image = $(this);
+	var source = (image).attr("src");
+	var alternate = (image).attr("alt");
+//	var alternate = $(this).attr("alt")
+	image.attr("src", alternate);
+	image.attr("alt", source);
+}
+
+$(document).on("click", ".foodImg", animate);
+
+$(document).on("click", ".cuisine", displayGifs);
